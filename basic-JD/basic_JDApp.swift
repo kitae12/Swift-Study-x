7598a0d4046aafd17c2b7e66e928fb0c90ce685b
kitae12 <77874046+kitae12@users.noreply.github.com>
2022-04-22 21:29:29 +0900

    Initial Commit

---
+++ basic-JD/basic_JDApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_JDApp.swift
+//  basic-JD
+//
+//  Created by Kitae on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct basic_JDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
